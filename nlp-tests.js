d597686ad8012ec0a35fab73bc68daddd62e66c4
Ian Munro <ianmunrobot@gmail.com>
2017-03-29 12:00:46 -0400

    adds sanity checking and character escaping

--- nlp-tests.js
+++ nlp-tests.js
@@ -3,15 +3,21 @@ const fs = require('fs')
 
 let jokes = JSON.parse(fs.readFileSync('badjokesSentences.json').toString()).jokes;
 
-jokes = jokes.map(joke => joke.replace('&ldquo;', '\"')
-  .replace('&rsquo;', '\'')
-  .replace('&hellip;', '...'))
+jokes = jokes.map(jokeLine => jokeLine
+    .replace('&ldquo;', '\"')
+    .replace('&rdquo;', '\"')
+    .replace('&quot;', '\"')
+    .replace('&rsquo;', '\'')
+    .replace('&hellip;', '...')
+  )
 
 const Markov = require('markov-strings');
 
 const markov = new Markov(jokes, {
   stateSize: 2,
   maxWords: 30,
+  minWords: 10,
+  minScore: 10,
 });
 markov.buildCorpusSync();
 const result = markov.generateSentenceSync();
